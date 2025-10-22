c7de43ebdad9acee4db59a4da5e69022bc9928e9
nadica.deleva <nadica.deleva@armedia.com>
2025-10-22 15:22:06 +0200

    Fix workflows

--- system-test/src/test/java/com/armedia/atddaccelerator/template/systemtest/smoketests/ApiSmokeTest.java
+++ system-test/src/test/java/com/armedia/atddaccelerator/template/systemtest/smoketests/ApiSmokeTest.java
@@ -1,4 +1,4 @@
-package com.optivem.atddaccelerator.template.systemtest.smoketests;
+package com.armedia.atddaccelerator.template.systemtest.smoketests;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
