bf8e7d3c916a31233eac14ddc5db219047e9fa6d
sowmiyathoguluva <s.thoguluva@armedia.com>
2025-10-28 19:09:33 -0400

    starting application locally

--- monolith/db-init/database.sql
+++ monolith/db-init/database.sql
@@ -1,7 +1,8 @@
 CREATE DATABASE IF NOT EXISTS flight_atdd;
 
-CREATE USER IF NOT EXISTS 'fl_advisor'@'%' IDENTIFIED BY 'fl_advisor';
+CREATE USER IF NOT EXISTS 'fl_advisor'@'%' IDENTIFIED WITH caching_sha2_password BY 'fl_advisor';
 
 GRANT ALL PRIVILEGES ON flight_atdd.* TO 'fl_advisor'@'%';
 
 FLUSH PRIVILEGES;
+
