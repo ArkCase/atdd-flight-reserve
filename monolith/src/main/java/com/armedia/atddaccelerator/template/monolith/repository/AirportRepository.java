dd11498be35badfec2cbf7e49584f430571075b4
sowmiyathoguluva <s.thoguluva@armedia.com>
2025-11-13 11:05:34 -0500

    new controllers and increase the wait time for application to start so that data can be imported at start

--- monolith/src/main/java/com/armedia/atddaccelerator/template/monolith/repository/AirportRepository.java
+++ monolith/src/main/java/com/armedia/atddaccelerator/template/monolith/repository/AirportRepository.java
@@ -2,9 +2,19 @@ package com.armedia.atddaccelerator.template.monolith.repository;
 
 import com.armedia.atddaccelerator.template.monolith.entity.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AirportRepository extends JpaRepository<Airport, Long> {
     Airport findByIata(String iata);
+
+    //List<Airport> findByCityIdAndCountryId(Integer cityId, Integer countryId);
+
+    @Query("SELECT a FROM Airport a WHERE a.home_city.id = :cityId AND a.home_country.id = :countryId")
+    List<Airport> findByCityAndCountry(@Param("cityId") Integer cityId,
+            @Param("countryId") Integer countryId);
 }
