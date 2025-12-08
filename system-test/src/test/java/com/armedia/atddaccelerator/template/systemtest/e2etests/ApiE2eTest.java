34d4a7c24bf31241147f53ca0bc4632e4577ef29
sowmiyathoguluva <s.thoguluva@armedia.com>
2025-12-08 18:17:05 -0500

    mocked external system test

--- system-test/src/test/java/com/armedia/atddaccelerator/template/systemtest/e2etests/ApiE2eTest.java
+++ system-test/src/test/java/com/armedia/atddaccelerator/template/systemtest/e2etests/ApiE2eTest.java
@@ -12,25 +12,48 @@ import java.net.http.HttpResponse;
 
 class ApiE2eTest {
 
+    static WireMockServer wireMockServer;
+
+    @BeforeAll
+    static void setup() {
+        wireMockServer = new WireMockServer(8080);
+        wireMockServer.start();
+
+        wireMockServer.stubFor(
+                get(urlEqualTo("/api/taxes/info"))
+                        .willReturn(aResponse()
+                                .withStatus(200)
+                                .withHeader("Content-Type", "application/json")
+                                .withBody("""
+                                    {
+                                      "data": {
+                                        "taxAmount": 1500,
+                                        "year": 2024,
+                                        "status": "OK"
+                                      }
+                                    }
+                                    """))
+        );
+    }
+
+    @AfterAll
+    static void tearDown() {
+        wireMockServer.stop();
+    }
+
     @Test
     void getTaxesFromExternalAPI_shouldReturnOK() throws Exception {
-        // DISCLAIMER: This is an example of a badly written test
-        // which unfortunately simulates real-life software test projects.
-        // This is the starting point for our ATDD Accelerator exercises.
-
-        // Arrange
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(new URI("http://localhost:8080/api/taxes/info")).GET().build();
-        // Act
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("http://localhost:8080/api/taxes/info"))
+                .GET()
+                .build();
+
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        // Assert
         assertEquals(200, response.statusCode());
-
-        String responseBody = response.body();
-
-        // Verify JSON structure contains expected fields
-        assertTrue(responseBody.contains("data"));
+        assertTrue(response.body().contains("data"));
+        assertTrue(response.body().contains("taxAmount"));
     }
 
     @Test
